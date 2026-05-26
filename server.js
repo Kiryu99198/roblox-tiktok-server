213f1296382b6c3adaeb345a4461fcc6d8244bbc
Kiryu99198 <kiryusetsuna1111@gmail.com>
2026-05-26 13:30:57 -0400

    Change TikTok username from 'shrubz' to 'coldheartedmk'

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Target username
-const TIKTOK_USERNAME = "shrubz"; 
+const TIKTOK_USERNAME = "coldheartedmk"; 
 
 let eventQueue = [];
 let pastFollowers = new Set(); 
